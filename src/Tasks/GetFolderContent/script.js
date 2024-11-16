c4f97982a590e95c9bdbf8fd5d792a4f445839a4
manganellidev <manganellidev@gmail.com>
2024-11-16 09:25:56 -0300

    update...

--- src/Tasks/GetFolderContent/script.js
+++ src/Tasks/GetFolderContent/script.js
@@ -1,17 +1,17 @@
-const tl = require('azure-pipelines-task-lib/task');
-const fs = require('fs');
-const path = require('path');
+const tl = require("azure-pipelines-task-lib/task");
+const fs = require("fs");
+const path = require("path");
+const chalk = require("chalk");
 
 try {
-    const parentFolder = tl.getInput('ParentFolder', true);
+  const parentFolder = tl.getInput("ParentFolder", true);
 
-    const files = fs.readdirSync(path.join(parentFolder));
+  const files = fs.readdirSync(path.join(parentFolder));
 
-    console.log('Content of '+parentFolder + ':');
-    for (const file of files) {
-        console.log('- ' + file);   
-    }
-    
+  console.log(chalk.blue("Content of " + parentFolder + ":"));
+  for (const file of files) {
+    console.log(chalk.green("- " + file));
+  }
 } catch (error) {
-    tl.setResult(tl.TaskResult.Failed, error);
+  tl.setResult(tl.TaskResult.Failed, error);
 }
