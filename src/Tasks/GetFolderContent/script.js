c29110f9c6ef9c5cf4276fc5e501f5b4a6d8692e
manganellidev <manganellidev@gmail.com>
2024-11-16 09:37:06 -0300

    update...

--- src/Tasks/GetFolderContent/script.js
+++ src/Tasks/GetFolderContent/script.js
@@ -1,16 +1,22 @@
 const tl = require("azure-pipelines-task-lib/task");
 const fs = require("fs");
 const path = require("path");
-const chalk = require("chalk");
+const dayjs = require("dayjs");
+const seedex = require("@mangadev-npm/seedex");
 
 try {
   const parentFolder = tl.getInput("ParentFolder", true);
 
   const files = fs.readdirSync(path.join(parentFolder));
+  const currentDate = dayjs().format("YYYY-MM-DD HH:mm:ss");
 
-  console.log(chalk.blue("Content of " + parentFolder + ":"));
+  console.log(seedex.generateRandomFirstName());
+  console.log(seedex.generateRandomLastName());
+  console.log(seedex.generateRandomEmailDomain());
+
+  console.log(`Listing files in "${parentFolder}" as of ${currentDate}:`);
   for (const file of files) {
-    console.log(chalk.green("- " + file));
+    console.log(`- ${file}`);
   }
 } catch (error) {
   tl.setResult(tl.TaskResult.Failed, error);
